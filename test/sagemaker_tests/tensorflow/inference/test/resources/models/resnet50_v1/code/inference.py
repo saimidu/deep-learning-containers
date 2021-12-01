1e7182a0c8945c1e870d78623903f41e3c096197
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-11-30 21:57:51 -0800

    Fix inference script

--- test/sagemaker_tests/tensorflow/inference/test/resources/models/resnet50_v1/code/inference.py
+++ test/sagemaker_tests/tensorflow/inference/test/resources/models/resnet50_v1/code/inference.py
@@ -1,26 +1,17 @@
-import json
-import numpy as np
-import json
 import io
-from PIL import Image
-import requests
-import os
-import io
-from PIL import Image
+
+import grpc
+import gzip
 import numpy as np
+import tensorflow as tf
 
-is_neuron_host = int(os.environ.get("NEURON_CORE_HOST_TOTAL", 0)) > 0
-print("IS NEURON HOST {}".format(is_neuron_host))
+from google.protobuf.json_format import MessageToJson
+from PIL import Image
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
 
-if is_neuron_host:
-    import grpc
-    import gzip
-    import tensorflow as tf
-    from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
-    from google.protobuf.json_format import MessageToJson
+prediction_services = {}
+compression_algo = gzip
 
-    prediction_services = {}
-    compression_algo = gzip
 
 def handler(data, context):
     f = data.read()
@@ -30,34 +21,24 @@ def handler(data, context):
     image = np.asarray(image.resize((224, 224)))
     image = np.concatenate([image[np.newaxis, :, :]] * batch_size)
 
-    if not is_neuron_host:
-        body = json.dumps({"signature_name": "serving_default", "instances": image.tolist()})
-        response = requests.post(context.rest_uri, data=body)
-        if response.status_code != 200:
-            raise ValueError(response.content.decode('utf-8'))
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'model'
+    request.model_spec.signature_name = 'serving_default'
+    request.inputs['images'].CopyFrom(
+        tf.compat.v1.make_tensor_proto(image, shape=image.shape, dtype=tf.float32))
 
-        response_content_type = context.accept_header
-        prediction = response.content
-        return prediction, response_content_type
-    else:
-        request = predict_pb2.PredictRequest()
-        request.model_spec.name = 'compiled_models'
-        request.model_spec.signature_name = 'serving_default'
-        request.inputs['images'].CopyFrom(
-            tf.compat.v1.make_tensor_proto(image, shape=image.shape, dtype=tf.float32))
+    # Call Predict gRPC service
+    result = get_prediction_service(context).Predict(request, 60.0)
+    print("Returning the response for grpc port: {}".format(context.grpc_port))
 
-        # Call Predict gRPC service
-        result = get_prediction_service(context).Predict(request, 60.0)
-        print("Returning the response for grpc port: %s" % (context.grpc_port))
+    # Return response
+    json_obj = MessageToJson(result)
+    return json_obj, "application/json"
 
-        # Return response
-        jsonObj = MessageToJson(result)
-        return jsonObj, "application/json"
 
 def get_prediction_service(context):
-    #global prediction_service
-    if not context.grpc_port in prediction_services:
-        #print("Creating service for port %s" % context.grpc_port)
+    # global prediction_service
+    if context.grpc_port not in prediction_services:
         channel = grpc.insecure_channel("localhost:{}".format(context.grpc_port))
         prediction_services[context.grpc_port] = prediction_service_pb2_grpc.PredictionServiceStub(channel)
     return prediction_services[context.grpc_port]
